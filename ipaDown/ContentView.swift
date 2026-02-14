3c6e5204ef25be3d0c0584eb7d61bccb4cd03644
ShawnRn <shawnrain.me@gmail.com>
2026-02-14 13:41:44 +0800

    Initial Commit

---
+++ ipaDown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ipaDown
+//
+//  Created by Shawn Rain on 2026/2/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ipaDown/ipaDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipaDownApp.swift
+//  ipaDown
+//
+//  Created by Shawn Rain on 2026/2/14.
+//
+
+import SwiftUI
+
+@main
+struct ipaDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
